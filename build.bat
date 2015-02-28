7b01a0440cf957b7d93c0ff7e0e93e6cd569fba7
Arialdo Martini <arialdomartini@gmail.com>
2015-02-28 23:32:46 +0100

    Screen is not cleared and no final pause

--- build.bat
+++ build.bat
@@ -1,8 +1,6 @@
 @echo off
-cls
 echo "Downloading FAKE..."
 "tools\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
 
 echo "Running build script"
 "packages\FAKE\tools\Fake.exe" build.fsx
-pause
